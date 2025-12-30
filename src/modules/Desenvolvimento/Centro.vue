0e70ea313f7d72300cc74791ca44d3f7ce9aa5f9
ThiagoNogueira2 <thiagonogueira_2003@hotmail.com>
2025-12-30 08:40:24 -0300

    feat: Atualizando responsivo do monsterparallax

--- src/modules/Desenvolvimento/Centro.vue
+++ src/modules/Desenvolvimento/Centro.vue
@@ -2,7 +2,7 @@
     <section class="section-contraste relative bg-[#0c0102]">
         <div class="relative overflow-hidden">
             <img src="../../assets/images/fundo.jpg" alt="contraste"
-                class="img-contraste w-full h-[400px] md:h-[700px] object-cover will-change-transform transform-gpu">
+                class="img-contraste w-full h-[400px] md:h-[800px] object-cover will-change-transform transform-gpu">
 
             <div
                 class="absolute inset-0 z-[1] bg-gradient-to-b from-[rgba(12,1,2,0)] from-[70%] to-[#0c0102] pointer-events-none">
